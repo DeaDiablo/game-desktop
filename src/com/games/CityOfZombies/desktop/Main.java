83566f073e80a5afbe40a3b420503be7bea089bc
DeaDiablo <deadiablo@gmail.com>
2014-08-16 16:27:09 +0400

    + create project

---
+++ src/com/games/CityOfZombies/desktop/Main.java
@@ -0,0 +1,21 @@
+package com.games.CityOfZombies.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.games.CityOfZombies.CityOfZombies;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    config.title = "City of zombies";
+    config.useGL30 = false;
+    config.width = 800;
+    config.height = 480;
+    config.fullscreen = false;
+    config.vSyncEnabled = false;
+    config.foregroundFPS = 0;
+    new LwjglApplication(new CityOfZombies(), config);
+  }
+}
